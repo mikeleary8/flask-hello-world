d3a0ceed54f8fd3bd9d688154764db9c3fddec38
mikeleary8 <73468923+mikeleary8@users.noreply.github.com>
2023-03-28 18:43:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request, json
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Test live build'
+
+    
+    url = "https://instagram-scraper-2022.p.rapidapi.com/ig/following/"
+
+    querystring = {"id_user":"44617448873"}
+
+    headers = {
+        "X-RapidAPI-Key": "02d25f21ecmsh6fd6238a435cabap1430c9jsna65bf3f1404e",
+        "X-RapidAPI-Host": "instagram-scraper-2022.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    jsonData = response.json()
+
+    return jsonData
