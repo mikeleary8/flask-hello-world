084fb13c4025e9a994869cae504af2f4d2d7a95a
mikeleary8 <73468923+mikeleary8@users.noreply.github.com>
2025-07-07 10:21:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,37 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/scraping/<id>', methods=["POST","GET"])
-def scraping(id):
 
-    nmi = request.args.get('nmi',  default = " ", type = str)
+@app.route('/retreat-webhook', methods=["POST"])
+def retreat_webhook(token):
 
-    if nmi != " ":
-        querystring = {"id_user":""+id+"", "next_max_id":""+nmi+""}
-    else:
-        querystring = {"id_user":""+id+""}
+    body = request.json
 
-    headers = {
-        "X-RapidAPI-Key": "f2923eedf0msh2990d552a2003d3p12f51bjsn663c397ea72b",
-        "X-RapidAPI-Host": "instagram-scraper-2022.p.rapidapi.com"
-    }
+    print(body)
 
-    url = "https://instagram-scraper-2022.p.rapidapi.com/ig/followers/"
-
-    
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    data = response.json()
-
-    #return data
-
-    return render_template('scraping.html', username=f'{id}', data=data)
-
-@app.route('/ig')
-def ig():
-    user_id = cl.user_id_from_username(ernestigler)
-
-    print(user_id)
-
-    return {}
+    return {}, 200
